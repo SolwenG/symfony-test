5bf31b26434dc1d99530aec7b9b9bfda3ba0215a
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2024-01-11 14:06:08 +0100

    added findAll produits in homepage

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Produit;
 use App\Form\AjoutProduitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,11 +14,18 @@ class ProduitsController extends AbstractController
 {
     #[Route('/produits', name: 'app_produits')]
 
-    public function new(Request $request): Response
+    public function new(Request $request, ManagerRegistry $doctrine): Response
     {
         $produit = new Produit();
         $form = $this->createForm(AjoutProduitType::class, $produit);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($produit);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_homepage', []);
+        }
 
         return $this->render('produits/index.html.twig', [
             'formProduit' => $form->createView(),
