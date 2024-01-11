084a2288aa843f2dc1c106d74ad026bbfbd2ba9c
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2024-01-11 11:51:28 +0100

    added formProduit

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\AjoutProduitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitsController extends AbstractController
 {
     #[Route('/produits', name: 'app_produits')]
-    public function index(): Response
+
+    public function new(Request $request): Response
     {
+        $produit = new Produit();
+        $form = $this->createForm(AjoutProduitType::class, $produit);
+        $form->handleRequest($request);
+
         return $this->render('produits/index.html.twig', [
-            'controller_name' => 'ProduitsController',
+            'formProduit' => $form->createView(),
         ]);
     }
 }
